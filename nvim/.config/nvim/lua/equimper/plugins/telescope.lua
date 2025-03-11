e4ec7a6e9ecfed4e7ef288d39545ce680f30da70
EQuimper <quimperemanuel@gmail.com>
2025-03-10 21:28:25 -0400

    Find in buffer

--- nvim/.config/nvim/lua/equimper/plugins/telescope.lua
+++ nvim/.config/nvim/lua/equimper/plugins/telescope.lua
@@ -31,7 +31,7 @@ return {
             ["<C-j>"] = actions.move_selection_next, -- move to next result
             ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
             ["<C-t>"] = trouble_telescope.open,
-          }
+          },
         },
       },
     })
@@ -42,9 +42,10 @@ return {
     local keymap = vim.keymap -- for conciseness
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Fuzzy find files from buffers" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-  end
+  end,
 }
