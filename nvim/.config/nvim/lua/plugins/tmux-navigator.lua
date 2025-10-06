06c48d70b370f936066b99cc6a22b2b48f719962
EQuimper <quimperemanuel@gmail.com>
2025-10-06 09:44:33 -0400

    fix tmux navigator

--- nvim/.config/nvim/lua/plugins/tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,36 +1,27 @@
 return {
   "christoomey/vim-tmux-navigator",
-  event = "VimEnter",
-  keys = {
-    {
-      "<c-h>",
-      "<cmd>TmuxNavigateLeft<cr>",
-      mode = { "n", "t" },
-      desc = "Move to left tmux pane",
-    },
-    {
-      "<c-j>",
-      "<cmd>TmuxNavigateDown<cr>",
-      mode = { "n", "t" },
-      desc = "Move to lower tmux pane",
-    },
-    {
-      "<c-k>",
-      "<cmd>TmuxNavigateUp<cr>",
-      mode = { "n", "t" },
-      desc = "Move to upper tmux pane",
-    },
-    {
-      "<c-l>",
-      "<cmd>TmuxNavigateRight<cr>",
-      mode = { "n", "t" },
-      desc = "Move to right tmux pane",
-    },
-    {
-      "<c-\\>",
-      "<cmd>TmuxNavigatePrevious<cr>",
-      mode = { "n", "t" },
-      desc = "Move to previous tmux pane",
-    },
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
   },
+  keys = {
+    { mode = "n", "<c-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { mode = "n", "<c-j>", "<cmd>TmuxNavigateDown<cr>" },
+    { mode = "n", "<c-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { mode = "n", "<c-l>", "<cmd>TmuxNavigateRight<cr>" },
+    { mode = "n", "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+    { mode = "t", "<c-h>", "<C-w><cmd>TmuxNavigateLeft<cr>" },
+    { mode = "t", "<c-j>", "<C-w><cmd>TmuxNavigateDown<cr>" },
+    { mode = "t", "<c-k>", "<C-w><cmd>TmuxNavigateUp<cr>" },
+    { mode = "t", "<c-l>", "<C-w><cmd>TmuxNavigateRight<cr>" },
+    { mode = "t", "<c-\\>", "<C-w><cmd>TmuxNavigatePrevious<cr>" },
+  },
+  init = function()
+    -- Disable default mappings to make it work in terminal mode
+    vim.g.tmux_navigator_no_mappings = 1
+  end,
 }
