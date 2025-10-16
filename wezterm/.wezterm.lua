4643eabc7f4b49a232b01e67db6ec4803b0a7b71
EQuimper <quimperemanuel@gmail.com>
2025-10-16 09:11:44 -0400

    better wezterm multiplexer

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,65 +1,153 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
 
 local config = wezterm.config_builder()
-
--- config.font = wezterm.font("MesloLGS Nerd Font Mono")
--- config.font = wezterm.font("JetbrainsMono Nerd Font Mono")
+workspace_switcher.apply_to_config(config)
 
 config.font_size = 13
-
-config.enable_tab_bar = false
 
 config.color_scheme = "Catppuccin Macchiato"
 
 config.window_decorations = "RESIZE"
--- config.window_background_opacity = 0.85
-config.window_background_opacity = 0.90
-config.macos_window_background_blur = 5
+config.window_background_opacity = 0.80
+config.macos_window_background_blur = 6
 
--- config.colors = {
--- 	foreground = "#CBE0F0",
--- 	background = "#011423",
--- 	cursor_bg = "#47FF9C",
--- 	cursor_border = "#47FF9C",
--- 	cursor_fg = "#011423",
--- 	selection_bg = "#033259",
--- 	selection_fg = "#CBE0F0",
--- 	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
--- 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
--- }
+config.window_frame = {
+	active_titlebar_bg = "#1e1e2e",
+	inactive_titlebar_bg = "#181825",
+}
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_max_width = 32
+config.show_new_tab_button_in_tab_bar = false
 
--- config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.colors = {
+	tab_bar = {
+		background = "#1e1e2e",
+		active_tab = {
+			bg_color = "#89b4fa",
+			fg_color = "#1e1e2e",
+			intensity = "Bold",
+		},
+		inactive_tab = {
+			bg_color = "#313244",
+			fg_color = "#cdd6f4",
+		},
+		inactive_tab_hover = {
+			bg_color = "#45475a",
+			fg_color = "#f5e0dc",
+		},
+		new_tab = {
+			bg_color = "#1e1e2e",
+			fg_color = "#89b4fa",
+		},
+		new_tab_hover = {
+			bg_color = "#89b4fa",
+			fg_color = "#1e1e2e",
+		},
+	},
+}
+
+config.leader = {
+	key = "a",
+	mods = "CMD",
+	timeout_milliseconds = 1000,
+}
+
+config.unix_domains = { {
+	name = "unix",
+} }
+config.default_gui_startup_args = { "connect", "unix" }
+
+local function is_vim(pane)
+	local process_info = pane:get_foreground_process_info()
+
+	local process_name = process_info and process_info.name
+
+	return process_name == "nvim" or process_name == "vim"
+end
+
+local direction_keys = {
+	Left = "h",
+	Down = "j",
+	Up = "k",
+	Right = "l",
+	-- reverse lookup
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
+local nav_keys = {
+	-- move between split panes
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
+	-- resize panes
+	split_nav("resize", "h"),
+	split_nav("resize", "j"),
+	split_nav("resize", "k"),
+	split_nav("resize", "l"),
+}
 
 config.keys = {
 	{
-		key = "|",
+		key = "d",
 		mods = "CMD",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "-",
-		mods = "CMD",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		action = wezterm.action.SplitHorizontal({
+			domain = "CurrentPaneDomain",
+		}),
 	},
 	{
 		key = "d",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.SplitVertical({
+			domain = "CurrentPaneDomain",
+		}),
+	},
+	{
+		key = "w",
 		mods = "CMD",
-		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+		action = wezterm.action.CloseCurrentPane({
+			confirm = true,
+		}),
 	},
 	{
 		key = "a",
 		mods = "LEADER|CTRL",
-		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+		action = wezterm.action.SendKey({
+			key = "a",
+			mods = "CTRL",
+		}),
 	},
 	{
 		key = "k",
 		mods = "CMD",
-		-- action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
-		action = wezterm.action.Multiple({
-			wezterm.action.ClearScrollback("ScrollbackAndViewport"),
-			wezterm.action.SendKey({ key = "L", mods = "CTRL" }),
-		}),
+		action = wezterm.action.SendString("clear\n"),
 	},
 	{
 		key = "p",
@@ -68,6 +156,56 @@ config.keys = {
 			alphabet = "1234567890",
 		}),
 	},
+	{
+		key = "s",
+		mods = "LEADER",
+		action = workspace_switcher.switch_workspace(),
+	},
+	{
+		key = "S",
+		mods = "LEADER",
+		action = workspace_switcher.switch_to_prev_workspace(),
+	},
+	{
+		key = "n",
+		mods = "LEADER",
+		action = wezterm.action.PromptInputLine({
+			description = "Enter name for new workspace",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:perform_action(
+						wezterm.action.SwitchToWorkspace({
+							name = line,
+						}),
+						pane
+					)
+				end
+			end),
+		}),
+	},
+	{
+		key = "r",
+		mods = "LEADER",
+		action = wezterm.action.PromptInputLine({
+			description = "Rename current tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+	{
+		key = "m",
+		mods = "LEADER",
+		action = wezterm.action.TogglePaneZoomState,
+	}, -- activate copy mode or vim mode
+	{
+		key = "Enter",
+		mods = "LEADER",
+		action = wezterm.action.ActivateCopyMode,
+	},
+	table.unpack(nav_keys),
 }
 
 return config
